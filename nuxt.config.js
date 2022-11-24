5884b4fbd7eeac7ff1868a13ebd26ed61bf884b1
ikun2019 <0215.miyatto@gmail.com>
2022-11-24 23:59:18 +0900

    i18nの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/sass/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -34,13 +35,15 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/i18n'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -48,7 +51,39 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: '/'
   },
-
+  vuetify: {
+    customVariables: ['~/assets/sass/variables.scss'],
+    treeShake: true,
+    themes: {
+      light: {
+        primary: '4080BE',
+        info: '4FC1E9',
+        success: '44D69E',
+        warning: 'FEB65E',
+        error: 'FB8678',
+        background: 'f6f6f4'
+      }
+    }
+  },
+  // Doc: https://nuxt-community.github.io/nuxt-i18n/basic-usage.html#nuxt-link
+  i18n: {
+    locales: ['ja', 'en'],
+    defaultLocale: 'ja',
+    // Doc: https://kazupon.github.io/vue-i18n/api/#properties
+    vueI18n: {
+      // 翻訳対象のキーがない場合に参照される言語
+      fallbackLocale: 'ja',
+      // trueの場合はi18nの警告を完全に表示しない。(default: false)
+      // silentTranslationWarn: true,
+      // フォールバック時に警告を発生させる。(default: false)
+      // true => 警告を発生させない（翻訳のキーが存在しない場合にのみ警告）
+      silentFallbackWarn: true,
+      messages: {
+        ja: require('./locales/ja.json'),
+        en: require('./locales/en.json')
+      }
+    }
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
